492daeed46846e40ea5ec1ef81ce298e55bb1527
Nivi0284 <nivijaiswal45@gmail.com>
2023-11-18 15:10:09 +0530

    modify correct code

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -7,6 +7,15 @@ public class F1 extends Car {
         super(name, 4, 2, 6, !isManual, "Formula 1", 1);
     }
 
+    public boolean isManual() {
+        // The F1 class inherits isManual from Car, but you might need to override it if necessary
+        return super.isManual();
+    }
+
+    public int getCurrentGear() {
+        return super.getCurrentGear();
+    }
+
     public void accelerate(int rate){
         int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
         /**
